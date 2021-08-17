967f422b40d25443e68518bb7fdbe0e8766e4fb0
toshiki <mochi.mochi.0397@gmail.com>
2021-08-17 16:50:14 +0900

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by 望月紀生 on 2021/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 望月紀生 on 2021/08/17.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
