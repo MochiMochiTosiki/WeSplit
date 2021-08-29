d71beb2e65c6abe28eee38accb889a546e574840
toshiki <mochi.mochi.0397@gmail.com>
2021-08-29 18:12:18 +0900

    Showing the player’s score with an alert

--- WeSplit/GuessTheFlag.swift
+++ WeSplit/GuessTheFlag.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct GuessTheFlag: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
 
     var body: some View {
         ZStack {
@@ -24,7 +26,7 @@ struct GuessTheFlag: View {
                     
                 ForEach(0 ..< 3) { number in
                     Button(action: {
-                        
+                        self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -34,6 +36,26 @@ struct GuessTheFlag: View {
                 Spacer()
             }
         }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
