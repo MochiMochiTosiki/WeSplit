b65714b7f51913c2994c26c87a682dff723755c1
toshiki <mochi.mochi.0397@gmail.com>
2021-08-31 00:06:00 +0900

    when they get an answer right or wrong, then display it in the alert.

--- WeSplit/GuessTheFlag.swift
+++ WeSplit/GuessTheFlag.swift
@@ -12,6 +12,7 @@ struct GuessTheFlag: View {
     @State private var scoreTitle = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var userScore = 0
 
     var body: some View {
         ZStack {
@@ -43,7 +44,7 @@ struct GuessTheFlag: View {
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(userScore) points"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
@@ -52,6 +53,7 @@ struct GuessTheFlag: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
             scoreTitle = "Wrong"
         }
